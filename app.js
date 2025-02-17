f5244713e2c8744aeb02c65be2b3bf4b53036032
Devharris <161379408+HarrisSagiris@users.noreply.github.com>
2025-02-17 16:41:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "/auth/github/callback"
+    callbackURL: "https://devit.to/auth/github/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
